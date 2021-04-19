368f96798d6bbdcb5ebeab49c2a37afdf9f458bc
zen.tang <zen.tang@bytedance.com>
2021-04-18 21:05:27 -0500

    add sentry and a home page

--- server.js
+++ server.js
@@ -1,12 +1,40 @@
-const express = require("express");
-const fs = require("fs");
-const path = require('path');
-const srt2vtt = require('srt-to-vtt')
-const ass2vtt = require('ass-to-vtt')
+import express from 'express'
+import * as fs from 'fs';
+import * as path from 'path'
+import srt2vtt from 'srt-to-vtt'
+import ass2vtt from 'ass-to-vtt'
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
+
+const app = express();
+
+Sentry.init({
+    dsn: "https://e44c1c573df2421b9ecf3cd073f7a10e@o574223.ingest.sentry.io/5725145",
+    integrations: [
+        // enable HTTP calls tracing
+        new Sentry.Integrations.Http({ tracing: true }),
+        // enable Express.js middleware tracing
+        new Tracing.Integrations.Express({ app }),
+    ],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+});
+
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
+
 
 // set the view engine to ejs
-const app = express();
 app.set('view engine', 'ejs');
+
+let __dirname = process.cwd();
+let allFiles = [];
 
 function encodeAllSRTFiles() {
     const directoryPath = 'assets/'
@@ -29,6 +57,9 @@ function encodeAllSRTFiles() {
                         .pipe(fs.createWriteStream('assets/' + name + '.vtt'))
                     console.log(`Written ${'assets/' + name}.vtt!`)
                 } else {
+                    if (ext === '.mp4') {
+                        allFiles.push(name)
+                    }
                     console.log('File:', filename)
                 }
             } catch (e) {
@@ -43,6 +74,16 @@ encodeAllSRTFiles()
 function getSafeVideoname(req) {
     return req.params.videoname;
 }
+
+app.get("/", function (req, res) {
+    res.render(__dirname + "/home", {
+        videos: allFiles
+    });
+});
+
+app.get("/debug-sentry", function mainHandler(req, res) {
+    throw new Error("My first Sentry error!");
+});
 
 app.get("/:videoname", function (req, res) {
     res.render(__dirname + "/index", {
@@ -91,6 +132,10 @@ app.get("/video/:videoname", function (req, res) {
     // Stream the video chunk to the client
     videoStream.pipe(res);
 });
+
+
+// The error handler must be before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
 
 
 app.listen(3000, function () {
